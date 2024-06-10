a4ef6293a68b685c0e928c0e23c30b6d2196c3c8
ozzyoss77 <crhistian.riverin@gmail.com>
2024-06-09 23:55:50 -0400

    feat: :art: adding health check

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import ingest from "./routes/ingestroutes";
 import chatModel from "./routes/chatmodelroutes";
 import SavePrompt from "./routes/promptroutes";
+import health from "./routes/health";
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use("/ingest", ingest);
 app.use("/ai", chatModel);
 app.use("/prompt", SavePrompt);
+app.use("/health", health);
 
 app.listen(config.port, () => {
   console.log(`Server running on http://${config.host}:${config.port}`);
