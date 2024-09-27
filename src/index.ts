685545f6d75d822257c81ab6bb39416bd6f96390
ozzyoss77 <crhistian.riverin@gmail.com>
2024-09-27 19:22:27 -0400

    feat: :art: minor fixes

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,39 @@
 import { config } from "./config";
 import express from "express";
+import helmet from "helmet";
+import {check,validationResult} from 'express-validator';
+import rateLimit from 'express-rate-limit';
 import ingest from "./routes/ingestroutes";
 import chatModel from "./routes/chatmodelroutes";
 import SavePrompt from "./routes/promptroutes";
 import health from "./routes/health";
 import SaveKey from "./routes/savekeyroutes";
 import Logger from "./utils/logger";
+import 'dotenv/config';
 
+// * Logger
 const logger = new Logger();
+
+// * Rate Limiter config
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutos
+  max: 100, // 100 peticiones por ventana
+  message: "Too many requests from this IP, please try again after 15 minutes."
+});
 
 const app = express();
 app.use(express.json());
 
+// * Helmet Security Middleware
+app.use(helmet());
+
+// * Rate Limiter Middleware
+app.use(limiter);
+
+// * CheckHealth Route
 app.use("/health", health);
 
-// middleware for token
+// * Middleware for token
 app.use((req, res, next) => {
   if (req.headers["apikey"] === config.apiKey) {
     next();
@@ -23,11 +42,29 @@ app.use((req, res, next) => {
   }
 });
 
-app.use("/ingest", ingest);
+// * Ingest Route
+// app.use("/ingest", ingest);
+app.post('/ingest', [
+  check('data').isString().trim().escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  }
+], ingest);
+
+// * Chat Route
 app.use("/ai", chatModel);
+
+// * Prompt Route
 app.use("/prompt", SavePrompt);
+
+// * Save Key Route
 app.use("/saveKey", SaveKey);
 
+// * Start Server
 app.listen(config.port, () => {
   logger.log(`Server running on http://${config.host}:${config.port}`);
 });
--- src/utils/errorhandler.ts
+++
@@ -1,8 +0,0 @@
-import Logger from "../utils/logger";
-
-const logger = new Logger();
-
-export function handleEmbeddingError(error: Error) {
-  logger.error(`Error al cargar los documentos en Qdrant: ${error}`);
-  throw new Error("Error al cargar los documentos en Qdrant");
-}
